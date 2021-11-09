e00dade8d52a40a846041f6cbe3a1dd7f45e2c14
inbenbox <inbenbox@qq.com>
2021-11-09 13:32:15 +0800

    before install vuejs-emoji for vue

---
+++ src/components/infoHeader.vue
@@ -0,0 +1,23 @@
+<templete>
+<div class="info-header">
+  <div>install</div>
+  <i>socket</i>
+  <i>name</i>
+  <i>room</i>
+</div>
+</templete>
+
+<script>
+export default {
+  name: "infoHeader"
+}
+</script>
+
+<style>
+.info-bar {
+  color: black;
+  background-color: red;
+  height: 100px;
+  width: 100px;
+}
+</style>
